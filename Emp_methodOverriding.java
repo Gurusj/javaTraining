44128c94d6d703b9e1390fae634eccf553ab111e
Gurusj <113494136+Gurusj@users.noreply.github.com>
2022-10-12 10:36:19 +0530

    Update Emp_methodOverriding.java

--- Emp_methodOverriding.java
+++ Emp_methodOverriding.java
@@ -1,55 +1,35 @@
 
 class Employee{
-
     int id;
     String name, address;
     String phone;
-
     public Employee(int i, String n, String a, String p){
         this.id = i;
         this.name = n;
         this.address = a;
         this.phone = p;
     }
-
-    //Overriding without parameters
     public void displayPhone(){
         System.out.println("ID: "+id+"\tPhone: "+phone);
     }
-
-    /*public void displayPhone(String phone){
-
-        System.out.println("\nPhone: "+phone);
-    }*/
 }
 
 class HR extends Employee{
     int salary;
-
     public HR(int i, String n, String a, String p, int sal) {
         super(i, n, a, p);
         this.salary = sal;
     }
-    //Overriding without parameters
-    //@Override
     public void displayPhone(){
-
         super.displayPhone();
         System.out.println("Name: "+name+"\tPhone: "+phone);
     }
 }
 
 class Emp_methodOverriding{
-
     public static void main(String args[]){
-
-        HR obj2 = new HR(1,"Abhishek","Savanur", "8660554576", 25000);
-        
-        //Without parameters 
+        HR obj2 = new HR(1,"Abhishek","Savanur", "8660554576", 25000); 
         obj2.displayPhone();
-
-        //with parameters 
-        //obj2.displayPhone("8986438922");
     }
 }
 
